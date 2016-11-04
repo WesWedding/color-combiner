5d700994b4acd4c746af9b43c41664c0113a229d
Weston Wedding <wes@wawco.com>
2016-11-03 22:46:45 -0700

    Basic functionality is in.

--- js/main.js
+++ js/main.js
@@ -2,26 +2,32 @@
 'use strict';
 var DEFAULT_FG = {values: [100, 100, 100]};
 var DEFAULT_BG = {values: [100, 100, 100]};
-var DEFAULT_ALPHA = {value: 0.3};
+var DEFAULT_ALPHA = {value: 0.5};
 
-var fgColor = DEFAULT_FG;
-var bgColor = DEFAULT_BG;
-var alpha = DEFAULT_ALPHA;
+var _fgColor = DEFAULT_FG;
+var _bgColor = DEFAULT_BG;
+var _alpha = DEFAULT_ALPHA;
 
-function pickOutRGBVals(colorString) {
-  var numbers = colorString.trim().toLowerCase();
-  numbers = numbers.split('(');
-  if (numbers.length > 1) {
-    numbers = numbers[1];
-  } else {
-    numbers = numbers[0];
+var _readStringFor = {
+  rgbVals: function(colorString) {
+    var numbers = colorString.trim().toLowerCase();
+    numbers = numbers.split('(');
+    if (numbers.length > 1) {
+      numbers = numbers[1];
+    } else {
+      numbers = numbers[0];
+    }
+    numbers = numbers.split(')')[0].split(',');
+    numbers = numbers.map(function(string) {
+      return string.trim();
+    });
+    return numbers;
+  },
+  alphaVals: function(alphaString) {
+    var number = parseFloat(alphaString.trim().toLowerCase());
+    return number;
   }
-  numbers = numbers.split(')')[0].split(',');
-  numbers = numbers.map(function(string) {
-    return string.trim();
-  });
-  return numbers;
-}
+};
 
 function rgbValuesAreValid(values) {
   var valid;
@@ -42,42 +48,65 @@ function setRgbVals(inputId, values) {
   var targetColor;
   switch(inputId) {
     case 'bgcolor':
-      targetColor = bgColor;
+      targetColor = _bgColor;
       break;
     case 'fgcolor':
-      targetColor = fgColor;
+      targetColor = _fgColor;
       break;
     default:
-      throw new Error("Invalid inputId.");
+      throw new Error("Invalid inputId: " + inputId);
       break;
   }
   if (!rgbValuesAreValid(values)) {
     throw new Error("Invalid values passed.");
   }
 
-  console.log("before", targetColor);
   targetColor.values = values;
-  console.log("after", targetColor);
+}
+function setAlphaVals(value) {
+  _alpha.value = value;
 }
 
 function onColorChange(event) {
-  var value = pickOutRGBVals(event.target.value);
-  setRgbVals(event.target.id, value);
+  var value = _readStringFor.rgbVals(event.target.value);
+  setRgbVals(event.target.name, value);
   setDocumentColors();
 }
 function onAlphaChange(event) {
-  console.log('alphachange', event);
+  var value = _readStringFor.alphaVals(event.target.value);
+  setAlphaVals(value);
+  setDocumentColors();
 }
 
 function setDocumentColors() {
-  document.body.style.background = colorToRGBString(bgColor);
+  var bgSwatch = document.querySelector('#bgcolor .swatch');
+  var fgSwatch = document.querySelector('#fgcolor .swatch');
+  var combinedSwatch = document.querySelector('#result .swatch');
+  bgSwatch.style.backgroundColor = colorToRGBString(_bgColor);
+  fgSwatch.style.backgroundColor = colorToRGBString(_fgColor);
+  fgSwatch.style.opacity = _alpha.value;
+  combinedSwatch.style.backgroundColor = colorToRGBString(combineColors(_bgColor, _fgColor, _alpha));
 }
 
 function colorToRGBString(color) {
-  var values = [];
-  if (!color.values && color.values.length === 3) {
-    values = color.values;
+  var colorStr = 'rgb(';
+  if (!color.values || color.values.length !== 3) {
+    throw new Error("colorToRGBString given an invalid color object");
   }
+  colorStr += color.values[0];
+  colorStr += ', ' + color.values[1];
+  colorStr += ', ' + color.values[2];
+  colorStr += ')';
+  return colorStr;
+}
+
+function combineColors(bgColor, fgColor, alpha) {
+  var color = {values: [100, 100, 100]};
+  color.values[0] = Math.floor((1 - alpha.value) * bgColor.values[0] + alpha.value * fgColor.values[0]);
+  color.values[1] = Math.floor((1 - alpha.value) * bgColor.values[1] + alpha.value * fgColor.values[1]);
+  color.values[2] = Math.floor((1 - alpha.value) * bgColor.values[2] + alpha.value * fgColor.values[2]);
+
+  return color;
 }
 
 function attachListenersToInputs() {
